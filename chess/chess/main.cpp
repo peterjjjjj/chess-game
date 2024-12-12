f0fd08cc9dbde806c2f0bbe2a8b6dd332d0091b2
mengfeijin <jin.xzy@outlook.com>
2024-12-12 15:20:07 -0800

    initialization

---
+++ chess/chess/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  chess
+//
+//  Created by mengfei jin on 12/12/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
